21f9dae63b63266ad3ff6f7de2cd067a6dd4b538
TWEETY <tweety_mza@hotmail.com>
2018-10-07 01:19:01 -0300

    Mejoras en el pie de página.
    Dejo los iconos de redes sociales como en el boceto y con un efecto más suave.
    Agrego otro ejemplo que nos puede servir.

--- sitio-lv/js/vellese.js
+++ sitio-lv/js/vellese.js
@@ -1,23 +1,5 @@
 //navbar
 
-$(document).ready(function () {
-  //Obtenemos las etiquetas <a> que tengan la Clase scroll
-  $("a.scroll").on('click', function (event) {
-    //Recuperamos el atributo data-target de la etiqueta <a>
-    var objetivo = $($(this).attr("data-target"));
-    //Si no esta vacio continuamos
-    if (objetivo.length) {
-      event.preventDefault();
-
-      $('html, body').animate({
-        scrollTop: $(objetivo).offset().top
-      }, 800, function () {
-
-      });
-    }
-  });
-});
-
 $(window).scroll(function () {
   /* affix after scrolling 100px */
   if ($(document).scrollTop() > 100) {
