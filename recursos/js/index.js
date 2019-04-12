9c0153752bad03f31a1eef06292f47ec37bfae5b
TWEETY <tweety_mza@hotmail.com>
2019-04-12 12:47:14 -0300

    Centrado de estiquers y limpieza de recursos.
    Moví todas las imágenes que no usamos a histórico.
    Cambié el nombre de las que usamos por uno más "formal" y los corregí donde se usan.
    Cambié el efecto de desenfoque de la imagen principal por un desenfoque menor.

--- recursos/js/index.js
+++ recursos/js/index.js
@@ -8,10 +8,10 @@ $(window).scroll(function() {
         $("#menu").removeClass("navbar2");
     }
     if ($("#logo").offset().top > 56) {
-        document.getElementById('logo').src = "recursos/index/nomadesoft-logo.png";
+        document.getElementById('logo').src = "recursos/index/nomadesoft-logo-completo-horizontal.png";
         $("#logo").addClass("navbar-logo");
     } else {
-        document.getElementById('logo').src = "recursos/index/nomadesoft-logo-completo.png";
+        document.getElementById('logo').src = "recursos/index/nomadesoft-logo-completo-vertical.png";
         $("#logo").removeClass("navbar-logo");
     }
     // mostramos u ocultamos flecha top segun donde estemos
