8057b3d2993cb98bc7c92137fc4f08bc2ea7edd0
David Senese <david.senese@gmail.com>
2019-04-13 22:15:00 -0300

    Arreglos index
    - Arregle el problema de la imagen cdo la pantalla es muy chica no se llega a ver la imagen completa.
    - Arregle igual q arriba el mapa de google maps para q tambien se achique segun el tamaño de la pantalla.
    - Ahora cdo el menu esta collapse y se selecciona una opcion se oculta automaticamente.
    - Borre varios comentarios.

--- recursos/js/index.js
+++ recursos/js/index.js
@@ -1,10 +1,7 @@
 $(window).scroll(function() {
-    //window.alert("scroll");
     if ($("#menu").offset().top > 56) {
-        //window.alert("scroll1");
         $("#menu").addClass("navbar2");
     } else {
-        //window.alert("scroll2");
         $("#menu").removeClass("navbar2");
     }
     if ($("#logo").offset().top > 56) {
@@ -20,51 +17,6 @@ $(window).scroll(function() {
     } else {
         document.getElementById('button_top').style.display = "none";
     }
-    // aplicamos efecto desenfocado a las secciones
-    // Seccion Productos
-    // var inicio = $("#productos").offset().top - 100
-    // var fin = inicio + ($("#productos")[0].clientHeight / 2)
-    // if (($(this).scrollTop() < inicio) 
-    //     || ($(this).scrollTop() > fin)) {
-    //     $("#productosdiv").addClass("ns-desenfoque");
-    //     $("#productosdiv").removeClass("ns-enfoque");
-    // } else {
-    //     $("#productosdiv").removeClass("ns-desenfoque");
-    //     $("#productosdiv").addClass("ns-enfoque");
-    // }
-    // // Seccion Servicios
-    // var inicio = $("#servicios").offset().top - 100
-    // var fin = inicio + ($("#servicios")[0].clientHeight / 2)
-    // if (($(this).scrollTop() < inicio) 
-    //     || ($(this).scrollTop() > fin)) {
-    //     $("#serviciosdiv").addClass("ns-desenfoque");
-    //     $("#serviciosdiv").removeClass("ns-enfoque");
-    // } else {
-    //     $("#serviciosdiv").removeClass("ns-desenfoque");
-    //     $("#serviciosdiv").addClass("ns-enfoque");
-    // }
-    // // Seccion Nosotros
-    // var inicio = $("#nosotros").offset().top - 100
-    // var fin = inicio + ($("#nosotros")[0].clientHeight / 2)
-    // if (($(this).scrollTop() < inicio) 
-    //     || ($(this).scrollTop() > fin)) {
-    //     $("#nosotrosdiv").addClass("ns-desenfoque");
-    //     $("#nosotrosdiv").removeClass("ns-enfoque");
-    // } else {
-    //     $("#nosotrosdiv").removeClass("ns-desenfoque");
-    //     $("#nosotrosdiv").addClass("ns-enfoque");
-    // }
-    // // Seccion Casos de Exito
-    // var inicio = $("#clientes").offset().top - 100
-    // var fin = inicio + ($("#clientes")[0].clientHeight / 2)
-    // if (($(this).scrollTop() < inicio) 
-    //     || ($(this).scrollTop() > fin)) {
-    //     $("#clientesdiv").addClass("ns-desenfoque");
-    //     $("#clientesdiv").removeClass("ns-enfoque");
-    // } else {
-    //     $("#clientesdiv").removeClass("ns-desenfoque");
-    //     $("#clientesdiv").addClass("ns-enfoque");
-    // }
 });
 
 $('.carousel').carousel({
@@ -87,6 +39,7 @@ $(document).ready(function() {
         var target = item.getAttribute('data-target');
         // navegamos
         $(target).get(0).scrollIntoView({block: "start", behavior: "smooth"});
+        $('.navbar-collapse').collapse('hide');
     });
 
 })
